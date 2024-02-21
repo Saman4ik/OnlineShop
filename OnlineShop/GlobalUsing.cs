a0d3e00c52aa24969209dd229b49df29b3d64760
Saman4ik <saman4ikdev@gmail.com>
2024-02-21 12:33:18 +0500

    Program cs ga qoshildi

--- OnlineShop/GlobalUsing.cs
+++ OnlineShop/GlobalUsing.cs
@@ -2,3 +2,4 @@
 global using OnlineShop.Data.Interface;
 global using Microsoft.EntityFrameworkCore;
 global using OnlineShop.Data;
+global using OnlineShop.Data.Repository;
--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -3,6 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb")));
 
+builder.Services.AddTransient<ICategoryInterface, CategoryRepository>();
+builder.Services.AddTransient<IImageInterface, ImageRepository>();
+builder.Services.AddTransient<IOrderItmesInterface, OrderItmesRepository>();
+builder.Services.AddTransient<IOrdersInterface, OrderRepository>();
+builder.Services.AddTransient<IProductInterface, ProductRepository>();
+builder.Services.AddTransient<ISubCategoryInterface, SubCategoryRepository>();
+builder.Services.AddTransient<IUserInterface, UserRepository>();
+
+
+
+
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
