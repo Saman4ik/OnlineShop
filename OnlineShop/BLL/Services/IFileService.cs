7bf10a5a9b378b063b48f1dad712f3c8c0bce444
Saman4ik <saman4ikdev@gmail.com>
2024-02-23 13:57:03 +0500

    Hali ancha kamchliklar bor category paneli qoshildi Hurmatli Somsachi uka buzib yuborman iltimos

---
+++ OnlineShop/BLL/Services/IFileService.cs
@@ -0,0 +1,29 @@
+﻿
+namespace OnlineShop.BLL.Services;
+public class IFileService(IWebHostEnvironment webHostEnvironment)
+    : Interfaces.IFileService
+{
+    private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
+
+    public void DeledeImage(string filename)
+    {
+        var wwwrootFolder = _webHostEnvironment.WebRootPath;
+        var filePath = Path.Combine(wwwrootFolder, filename.Replace("~/", ""));
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
+
+    public string UploadImage(IFormFile file)
+    {
+        var wwwrotFolder = _webHostEnvironment.WebRootPath;
+        var uniqeFileName = Guid.NewGuid().ToString() + "_" + file.Name;
+        var filePath = Path.Combine(wwwrotFolder + "/images/", uniqeFileName);
+        using (var fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            file.CopyTo(fileStream);
+        }
+        return $"~/images/{uniqeFileName}";
+    }
+}
